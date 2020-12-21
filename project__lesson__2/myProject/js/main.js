a0e46e0bd18a7a3bdd7f624174ec12e1fe3c20b1
Alex <alexhoropchanu@gmail.com>
2020-12-21 19:31:10 +0200

    finishedDZ

--- project__lesson__2/myProject/js/main.js
+++ project__lesson__2/myProject/js/main.js
@@ -1,30 +1,32 @@
 class ProductsList{
     constructor(container = '.products'){
         this.container = container;
-        this._fetchProducts();
         this.goods = [];//массив товаров
 //        this.allProducts=[];//массив объектов
+        this._fetchProducts();
     }
     _fetchProducts(){
         this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
+            {id: 1, title: 'Notebook', price: 3000},
             {id: 2, title: 'Mouse', price: 20},
             {id: 3, title: 'Keyboard', price: 200},
             {id: 4, title: 'Gamepad', price: 50},
         ];
+        // console.log(this.goods[0].price + this.goods[1].price + this.goods[2].price + this.goods[3].price);
     }
-    
+    goodList(){
+        console.log(this.goods[0].price + this.goods[1].price + this.goods[2].price + this.goods[3].price);
+    }
+
+
     render(){
         const block = document.querySelector(this.container);
         for(let product of this.goods){
             const productObject = new ProductItem(product);
 //            this.allProducts.push(productObject);
 //            block.innerHTML += productObject.render();
-              block.insertAdjacentHTML('beforeend',productObject.render());
+            block.insertAdjacentHTML('beforeend',productObject.render());
         }
-    }
-    sumProduct(){
-            console.log(this.goods[0].price + this.goods[1].price + this.goods[2].price + this.goods[3].price);
     }
 }
 
@@ -37,7 +39,7 @@ class ProductItem{
     }
     render(){
         return `<div class="product-item">
-                <img src="${this.img}">
+                <img src="${this.img}" alt="Pictures">
                 <h3>${this.title}</h3>
                 <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
@@ -47,25 +49,24 @@ class ProductItem{
 
 let list = new ProductsList();
 list.render();
-list.sumProduct();
-
-class Card{
-    constructor() {
-    }
-    addProduct(){
-
-    }
-    removeProduct(){
-
-    }
-}
-class CardItem{
-    constructor() {
-    }
-    iterationProduct(){
-
-    }
-    removeIterationProduct(){
-
-    }
-}
+list.goodList();
+// class Card{
+//     constructor() {
+//     }
+//     addProduct(){
+//
+//     }
+//     removeProduct(){
+//
+//     }
+// }
+// class CardItem{
+//     constructor() {
+//     }
+//     iterationProduct(){
+//
+//     }
+//     removeIterationProduct(){
+//
+//     }
+// }
